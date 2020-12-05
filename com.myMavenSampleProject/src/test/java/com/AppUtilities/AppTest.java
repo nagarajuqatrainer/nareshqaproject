01f580f84c8a583958cdea477c44933e8db4b940
nagar <nagar@LAPTOP-6OACLVLF>
2020-12-05 07:14:33 +0530

    First Commit

---
+++ com.myMavenSampleProject/src/test/java/com/AppUtilities/AppTest.java
@@ -0,0 +1,27 @@
+package com.AppUtilities;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class AppTest 
+{
+	
+	public static WebDriver driver;
+	public static String weburl ="https://www.mycontactform.com/";
+	//mvn archetype:generate
+	//enter mvn project folder using cmd
+	
+    @Test
+    public void verifysamplemaventest() throws Exception
+    {
+     
+    	
+    System.out.println("Run maven sample test");
+    	
+    	
+    	
+    	
+    }
+}
